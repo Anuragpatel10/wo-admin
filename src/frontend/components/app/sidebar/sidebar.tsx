e90eedfa2bb6c00629075ba24ab46cc4ae83f180
Anurag Patel <anurag.patel@wobot.ai>
2025-07-28 16:40:36 +0530

    hide sidebar

--- src/frontend/components/app/sidebar/sidebar.tsx
+++ src/frontend/components/app/sidebar/sidebar.tsx
@@ -56,7 +56,7 @@ const SidebarOriginal: React.FC<Props> = (props) => {
   ])
 
   return (
-    <StyledSidebar className={isVisible ? 'visible' : 'hidden'} data-css="sidebar">
+    <StyledSidebar className='hidden' data-css="sidebar">
       <SidebarBranding branding={branding} />
       <Box flexGrow={1} className={cssClass('Resources')} data-css="sidebar-resources">
         <SidebarResourceSection resources={resources} />
--- src/frontend/components/app/version.tsx
+++ src/frontend/components/app/version.tsx
@@ -1,49 +1,65 @@
 import React from 'react'
 import styled from 'styled-components'
-import { cssClass, Text, Box } from '@adminjs/design-system'
+import {Box, cssClass, Link, Text} from '@adminjs/design-system'
 
-import { VersionProps } from '../../../adminjs-options.interface'
-import { useTranslation } from '../../hooks'
+import {VersionProps} from '../../../adminjs-options.interface'
+import {useTranslation} from '../../hooks'
 import allowOverride from '../../hoc/allow-override'
 
 export type Props = {
-  versions: VersionProps;
+    versions: VersionProps;
 }
 
 const VersionItem = styled(Text)`
-  padding: 12px 24px 12px 0;
+    padding: 12px 24px 12px 0;
 `
 
 VersionItem.defaultProps = {
-  display: ['none', 'block'],
-  color: 'grey100',
+    display: ['none', 'block'],
+    color: 'grey100',
 }
 
 const Version: React.FC<Props> = (props) => {
-  const { versions } = props
-  const { admin, app } = versions
+    const {versions} = props
+    const {admin, app} = versions
 
-  const { translateLabel } = useTranslation()
+    const {translateLabel} = useTranslation()
 
-  return (
-    <Box flex flexGrow={1} py="default" px="xxl" className={cssClass('Version')} data-css="version">
-      {admin && (
-        <VersionItem>
-          {translateLabel('adminVersion', { version: admin })}
-        </VersionItem>
-      )}
-      {app && (
-        <VersionItem>
-          {translateLabel('appVersion', { version: app })}
-        </VersionItem>
-      )}
-    </Box>
-  )
+    return (
+        <Box flex flexGrow={1} py="default" px="xxl">
+            <Link href="/">
+                <svg width="52" height="48" viewBox="0 0 52 48" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <rect
+                        width="11.0477"
+                        height="27.8427"
+                        rx="5.52383"
+                        transform="matrix(0.854439 -0.519552 0.510535 0.859857 0.499878 24.0586)"
+                        fill="#3766E8"
+                    />
+                    <rect
+                        width="11.0477"
+                        height="42.3694"
+                        rx="5.52383"
+                        transform="matrix(0.854439 -0.519552 0.510535 0.859857 12.3904 8.65234)"
+                        fill="#3766E8"
+                    />
+                    <rect
+                        width="11.0477"
+                        height="21.5479"
+                        rx="5.52383"
+                        transform="matrix(0.854439 -0.519552 0.510535 0.859857 31.0594 5.73828)"
+                        fill="#3766E8"
+                    />
+                </svg>
+            </Link>
+        </Box>
+    )
 }
 
 const OverridableVersion = allowOverride(Version, 'Version')
 
 export {
-  OverridableVersion as default,
-  OverridableVersion as Version,
+    OverridableVersion as default,
+    OverridableVersion as Version,
 }
